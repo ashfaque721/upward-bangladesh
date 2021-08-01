bf0af9753bf5b33b37174369600b9041a0acddd2
Ashphalt <asfaqueeshan721@gmail.com>
2021-08-01 19:04:01 +0600

    router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React, {Component} from 'react';
 import Pagewrapper from './components/pagewrapper';
-import Home from './components/pages/home';
 import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
-class App extends React.Component {
+
+// Pages
+import Home from './components/pages/home';
+import About from './components/pages/About';
+
+class App extends Component {
   render() {
     return (
       <Router>
         <Pagewrapper>
-          <Route path="/" component={Home}/>
+          <Route exact ={true} path="/" component={Home}
+          />
+
+          <Route path="/about" component={About}
+            
+          />
+
+
         </Pagewrapper>
       </Router>
     );
