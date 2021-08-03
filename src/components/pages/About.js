ed796e6d9a8b87ad5980c3e400e036eed5595a67
Ashphalt <asfaqueeshan721@gmail.com>
2021-08-03 11:47:42 +0600

    img

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import Header from '../common/Header';
-import 
+import image from '../assets/img/about.jpg'
 
 class About extends Component{
     render() {
