696f1a3dcfa30589f5555c6373a8e1c92461a846
Ashphalt <asfaqueeshan721@gmail.com>
2021-08-08 00:36:41 +0600

    redux and API

--- src/App.js
+++ src/App.js
@@ -1,30 +1,70 @@
 import React, { Component } from 'react';
 import Pagewrapper from './components/pagewrapper';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-
+import { connect } from 'react-redux';
 // Pages
 import Home from './components/pages/home';
 import About from './components/pages/About';
 import Contact from './components/pages/Contact';
+import Login from './components/pages/Login';
+import AdminWrapper from './components/AdminWrapper';
+import Dashboard from './components/pages/Dashboard';
 
 class App extends Component {
   render() {
     return (
       <Router>
-        <Pagewrapper>
-          <Route exact={true} path="/" component={Home}
+
+        <Route 
+          path="/admin"
+          render={props => {
+            return (
+              <AdminWrapper>
+                {this.props.auth.token? <Dashboard/> : <Login />}
+              </AdminWrapper>
+            )
+          }}
+        />
+
+        
+
+          <Route exact={true} path="/" render={props => (
+            <Pagewrapper>
+              <Home {...props}/>
+            </Pagewrapper>
+          )}
           />
 
-          <Route path="/about" component={About}
+          <Route path="/about" render={props => (
+            <Pagewrapper>
+              <About {...props}/>
+            </Pagewrapper>
+          )}
 
           />
 
-          <Route path="/contact" component={Contact}></Route>
+          <Route path="/contact" render={props => (
+            <Pagewrapper>
+              <Contact {...props}/>
+            </Pagewrapper>
+          )}/>
 
-        </Pagewrapper>
+        
       </Router>
     );
   }
 }
 
-export default App;
+const mapStateToProps = state => {
+  return {
+    auth: state.auth
+  }
+}
+
+const mapDispatchToProps = dispath => {
+  return {
+
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
---
+++ src/store/actions/authActions.js
@@ -0,0 +1,26 @@
+import API from '../../utils/api';
+
+export const login = (email, pass) => {
+    return (dispatch) => {
+        API.login(email, pass, res => {
+            console.log("Result", res.data);
+            dispatch({
+                type: 'LOGIN',
+                payload: {email, pass}
+            })
+        })
+    }
+    /*
+    return {
+        type: 'LOGIN',
+        payload: {email, pass}
+    }
+    */
+}
+
+export const register = (email, pass) => {
+    return {
+        type: 'REGISTER',
+        payload: {email, pass}
+    }
+}
