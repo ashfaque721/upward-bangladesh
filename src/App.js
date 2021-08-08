d606ebaafd94c7892696f662a4edd13dd8d5b307
Ashphalt <asfaqueeshan721@gmail.com>
2021-08-08 17:03:15 +0600

    persistor

--- src/App.js
+++ src/App.js
@@ -2,21 +2,63 @@ import React, { Component } from 'react';
 import Pagewrapper from './components/pagewrapper';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { connect } from 'react-redux';
+
 // Pages
 import Home from './components/pages/home';
 import About from './components/pages/About';
 import Contact from './components/pages/Contact';
 import Login from './components/pages/Login';
+
+// Admin Pages
+import Dashboard from './components/pages/Admin/Dashboard';
+import Users from './components/pages/Admin/Users';
+import Posts from './components/pages/Admin/Posts';
+
 import AdminWrapper from './components/AdminWrapper';
 import LoginWrapper from './components/LoggingWrapper';
-import Dashboard from './components/pages/Dashboard';
 
 class App extends Component {
   render() {
     return (
       <Router>
-
+        <Route
+          path="/admin/users"
+          render={props => {
+            return (
+              <div>
+                {this.props.auth.token?
+                  <AdminWrapper>
+                    <Users/>
+                  </AdminWrapper> 
+                :
+                  <LoginWrapper>
+                    <Login />
+                  </LoginWrapper>
+                }
+              </div>
+            )
+          }}
+        />
+        <Route
+          path="/admin/posts"
+          render={props => {
+            return (
+              <div>
+                {this.props.auth.token?
+                  <AdminWrapper>
+                    <Posts/>
+                  </AdminWrapper> 
+                :
+                  <LoginWrapper>
+                    <Login />
+                  </LoginWrapper>
+                }
+              </div>
+            )
+          }}
+        />
         <Route 
+          exact={true}
           path="/admin"
           render={props => {
             return (
--- src/index.js
+++ src/index.js
@@ -3,22 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {createStore, applyMiddleware} from 'redux';
 import rootReducer from './store/reducers';
 import {Provider} from 'react-redux';
-import thunk from 'redux-thunk';
 
+import configureStore from './store/configureStore';
+import {PersistGate} from 'redux-persist/integration/react';
 
-const store = createStore(
-    rootReducer,
-    applyMiddleware(thunk)
-  );
-
-
+const {store, persistor} = configureStore();
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>
   , document.getElementById('root'));
 
