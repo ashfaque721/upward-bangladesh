76509cc2707a6fbb202da743e588d63a9052e509
Ashphalt <asfaqueeshan721@gmail.com>
2021-08-15 13:32:31 +0600

    field Width

--- src/components/common/Field.js
+++ src/components/common/Field.js
@@ -7,7 +7,7 @@ class Field extends Component {
                 {this.props.elementName === 'input' ?
                     <input className="form-control" id={this.props.name} type={this.props.type} placeholder={this.props.placeholder} data-sb-validations="required" name={this.props.name} onChange={this.props.onChange} onBlur={this.props.onBlur} />
                     :
-                    <textarea className="form-control" id={this.props.name} placeholder={this.props.placeholder} data-sb-validations="required" name={this.props.name} onChange={this.props.onChange} onBlur={this.props.onBlur} />
+                    <textarea className="form-control" style={{height: "250px"}} id={this.props.name} placeholder={this.props.placeholder} data-sb-validations="required" name={this.props.name} onChange={this.props.onChange} onBlur={this.props.onBlur} />
                 }
                 <p className='help-block text-danger'>
                     {(this.props.touched && this.props.errors) &&
