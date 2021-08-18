54d2fe664aae3dd781d21f61a769c8dbacc3379b
Ashphalt <asfaqueeshan721@gmail.com>
2021-08-18 12:42:46 +0600

    gallery

---
+++ src/components/pages/Gallery.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import img1 from '../assets/img/gallery/1.jpg';
+import img1 from '../assets/img/gallery/1.jpg';
+import img1 from '../assets/img/gallery/1.jpg';
+import img1 from '../assets/img/gallery/1.jpg';
+
+const Gallery = () => {
+
+    let data = [
+        {
+            id: 1,
+            imgSrc: '',
+        },
+        {
+            id: 2,
+            imgSrc: '',
+        },
+        {
+            id: 3,
+            imgSrc: '',
+        },
+        {
+            id: 4,
+            imgSrc: '',
+        },
+        {
+            id: 5,
+            imgSrc: '',
+        },
+    ]
+
+    return (
+        <h1>Gallery</h1>
+    )
+}
+
+export default Gallery;
