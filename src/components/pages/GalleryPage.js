85323fc8005ea23e430a0266df12688600421583
Ashphalt <asfaqueeshan721@gmail.com>
2021-08-18 12:43:14 +0600

    img

---
+++ src/components/pages/GalleryPage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Gallery.css';
+import Gallery from './Gallery';
+function GalleryPage () {
+    return (
+    <>
+        <h1 style={{textAlign: 'center'}}>Our Gallery</h1>
+        <Gallery />
+    </>
+    );
+}
+
+export default GalleryPage
