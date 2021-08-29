00a9987b22e9bf63af2a566fa6548cc86f190ffc
Ashphalt <asfaqueeshan721@gmail.com>
2021-08-29 13:31:37 +0600

    ok

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import {HashRouter as Router, Route} from 'react-router-dom';
 // Pages
 import Home from './components/pages/home';
 import About from './components/pages/About';
